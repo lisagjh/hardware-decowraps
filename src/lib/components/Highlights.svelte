891614f27a3ec5ccd1caf5d8fa0beca0f700c682
lisagjh <lisagjhoorn@outlook.com>
2025-10-24 16:38:01 +0200

    Update highlights layout and styling for desktop
    Improves the Highlights component's desktop layout by switching to a horizontal flex row, updating details and summary styling, and enhancing image and button presentation. These changes provide a more visually appealing and interactive experience for larger screens.

--- src/lib/components/Highlights.svelte
+++ src/lib/components/Highlights.svelte
@@ -34,36 +34,36 @@
 
 <section id="highlights" class="highlights">
 	<h2>Highlights</h2>
-    <p>Take a look at some of our personal favorites - just a small peek into our vast collection. </p>
+	<p>Take a look at some of our personal favorites - just a small peek into our vast collection.</p>
 
 	<div class="wrapper">
-	    {#each themes as theme, index}
-    		<details
-    			class="highlight"
-    			style="--theme-color: var(--{theme.color}); --theme-color-dark: var(--{theme.color}-dark); --theme-color-light: var(--{theme.color}-light);"
-    			open={index === 0}
-    			name="themes"
-    		>
-    			<summary class="text">
-    				<h3>{theme.name}</h3>
-    				<p><span>{theme.name}</span></p>
-    			</summary>
-    			<div class="content">
-    				<p>{theme.description}</p>
-    				<img src={theme.image} alt={theme.alt} />
-    				<button>See More</button>
-    			</div>
-    		</details>
-    	{/each}
+		{#each themes as theme, index}
+			<details
+				class="highlight"
+				style="--theme-color: var(--{theme.color}); --theme-color-dark: var(--{theme.color}-dark); --theme-color-light: var(--{theme.color}-light);"
+				open={index === 0}
+				name="themes"
+			>
+				<summary class="text">
+					<h3>{theme.name}</h3>
+					<p><span>{theme.name}</span></p>
+				</summary>
+				<div class="content">
+					<p>{theme.description}</p>
+					<img src={theme.image} alt={theme.alt} />
+					<button>See More</button>
+				</div>
+			</details>
+		{/each}
 	</div>
 </section>
 
 <style>
-    .highlights {
-        display: flex;
-        flex-direction: column;
-        gap: 1rem;
-    }
+	.highlights {
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+	}
 
 	.wrapper {
 		display: flex;
@@ -139,44 +139,94 @@
 	}
 
 	@media (width > 47rem) {
-        .wrapper {
-            flex-direction: row;
-            overflow: scroll;
-        }
-
-		details {
-            display: flex;
+		.wrapper {
 			flex-direction: row;
-            width: fit-content;
+			height: 500px; /* Of een andere gewenste hoogte */
 		}
 
-        summary {
-            position: relative;
-            width: fit-content;
-            padding: 1rem .5rem;
+		details {
+			display: flex;
+			flex-direction: row;
+			transition: flex 0.3s ease;
+			flex: 0 0 auto;
+		}
 
-            & h3 {
-                font-family: var(--font-family);
-                font-size: .85rem;
+		details:not([open]) {
+			flex: 0 0 auto;
+			width: auto;
+		}
+
+		details[open] {
+			flex: 1;
+		}
+
+		summary {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			justify-content: end;
+			padding: 1rem 0.5rem;
+			cursor: pointer;
+			position: relative;
+
+			& h3 {
+				position: absolute;
+				top: 0;
+				left: 50%;
+				translate: -50% 1rem;
+				font-size: 0.85rem;
+				font-weight: 300;
+			}
+
+			& p {
+				white-space: nowrap;
+				font-size: 1.8rem;
+				font-weight: 800;
+				margin: 0;
+				writing-mode: vertical-rl;
+				transform: rotate(180deg);
+			}
+		}
+
+		img {
+			max-width: 30rem;
+			object-fit: contain;
+		}
+
+		.content {
+			background-color: var(--theme-color-light);
+			display: flex;
+			flex-direction: column;
+			gap: 1rem;
+			padding: 2rem;
+			width: 100%;
+			min-height: fit-content;
+			height: 100%;
+			transition: all 0.3s ease;
+			overflow-y: auto;
+
+            & img {
+                height: 100%;
+                width: 100%;
+                max-width: 100%;
+                object-fit: cover;
+
             }
 
-            & p {
-                position: absolute;
-                transform: rotate(-90deg);
-                bottom: 2rem;
-                left: 50%;
-                translate: -50% -50%;
-                font-size: 2.5rem;
-                text-transform: uppercase;
-            }
-        }
+			& button {
+				background-color: var(--theme-color-dark);
+				color: var(--neutral-100);
+				padding: 0.75rem 1.5rem;
+				width: fit-content;
+				border: none;
+				font-weight: 400;
+				align-self: flex-end;
+				cursor: pointer;
+			}
+		}
 
-        img {
-            width: 100%;
-        }
+		details[open] .content {
+			height: 100%;
+		}
 	}
-
-    .highlights {
-        margin-bottom: 100rem;
-    }
 </style>
